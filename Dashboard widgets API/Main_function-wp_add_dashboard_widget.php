2a09af01a946d5b8f36d983329dc664d0ccdff49
kipmyk <kipmyk@gmail.com>
2024-01-23 09:02:22 +0300

    Updatd: Dashboard widgets API

--- Dashboard widgets API/Main_function-wp_add_dashboard_widget.php
+++ Dashboard widgets API/Main_function-wp_add_dashboard_widget.php
@@ -13,3 +13,52 @@ add_action( 'wp_dashboard_setup', 'wpdocs_add_dashboard_widgets' );
 function dashboard_widget_function( $post, $callback_args ) {
 	esc_html_e( "Hello World, this is my first Dashboard Widget!", "textdomain" );
 }
+
+function wp_add_dashboard_widget( $widget_id, $widget_name, $callback, $control_callback = null, $callback_args = null, $context = 'normal', $priority = 'core' ) {
+	global $wp_dashboard_control_callbacks;
+
+	$screen = get_current_screen();
+
+	$private_callback_args = array( '__widget_basename' => $widget_name );
+
+	if ( is_null( $callback_args ) ) {
+		$callback_args = $private_callback_args;
+	} elseif ( is_array( $callback_args ) ) {
+		$callback_args = array_merge( $callback_args, $private_callback_args );
+	}
+
+	if ( $control_callback && is_callable( $control_callback ) && current_user_can( 'edit_dashboard' ) ) {
+		$wp_dashboard_control_callbacks[ $widget_id ] = $control_callback;
+
+		if ( isset( $_GET['edit'] ) && $widget_id === $_GET['edit'] ) {
+			list($url)    = explode( '#', add_query_arg( 'edit', false ), 2 );
+			$widget_name .= ' <span class="postbox-title-action"><a href="' . esc_url( $url ) . '">' . __( 'Cancel' ) . '</a></span>';
+			$callback     = '_wp_dashboard_control_callback';
+		} else {
+			list($url)    = explode( '#', add_query_arg( 'edit', $widget_id ), 2 );
+			$widget_name .= ' <span class="postbox-title-action"><a href="' . esc_url( "$url#$widget_id" ) . '" class="edit-box open-box">' . __( 'Configure' ) . '</a></span>';
+		}
+	}
+
+	$side_widgets = array( 'dashboard_quick_press', 'dashboard_primary' );
+
+	if ( in_array( $widget_id, $side_widgets, true ) ) {
+		$context = 'side';
+	}
+
+	$high_priority_widgets = array( 'dashboard_browser_nag', 'dashboard_php_nag' );
+
+	if ( in_array( $widget_id, $high_priority_widgets, true ) ) {
+		$priority = 'high';
+	}
+
+	if ( empty( $context ) ) {
+		$context = 'normal';
+	}
+
+	if ( empty( $priority ) ) {
+		$priority = 'core';
+	}
+
+	add_meta_box( $widget_id, $widget_name, $callback, $screen, $context, $priority, $callback_args );
+}
